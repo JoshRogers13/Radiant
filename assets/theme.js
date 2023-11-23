ba562cb4854962e59ae4f5f8e3c7c6f46c2666ba
JoshRogers13 <56076198+JoshRogers13@users.noreply.github.com>
2023-11-23 21:08:21 +0000

    Test header scroll

--- assets/theme.js
+++ assets/theme.js
@@ -1,14 +1,15 @@
 // Show header on scroll up or top of the page
 var header = $('#store--header');
 var lastScrollTop = 0;
+var headerHeight = header.outerHeight(true);
 
 $(window).scroll(function(){
     var st = $(this).scrollTop();
 
-    if (st > lastScrollTop ){
-        header.removeClass('sticky').addClass('headHide');
-    } else if (st == 0) {
+    if (st == 0 ){
         header.removeClass('sticky', 'headHide');
+    } else if (st > lastScrollTop) {
+        header.removeClass('sticky').addClass('headHide');
     } else if ( st < lastScrollTop ){
         header.removeClass('headHide').addClass('sticky');
     }
